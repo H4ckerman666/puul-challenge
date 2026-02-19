5eff2ed091aaa0f3420116d8e2ac9935c107f558
H4ckerman <paletovicion@hotmail.com>
2026-02-19 14:52:03 -0600

    Imports Prisma and Tasks modules
    Imports the Prisma and Tasks modules into the application module.
    This enables the application to use the Prisma client for database
    interactions and the Tasks module for managing tasks.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PrismaModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
